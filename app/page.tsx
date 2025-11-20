e2084eee86bd20b6e01ce5e0ee5dce020d51bdcb
Anas Ali <anasali.techxpert@gmail.com>
2025-11-20 11:31:50 +0500

    Remove unused import in Home component and add empty sign-in and sign-up pages

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 // import Features from '@/components/Features';
 import Footer from '@/components/Footer';
-
 export default function Home() {
   return (
     <div className="min-h-screen bg-white dark:bg-black">
